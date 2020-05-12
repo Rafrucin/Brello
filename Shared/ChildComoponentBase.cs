a5a5724f72c8b8b3b542d36c226ec1eb82b4c723
Rafał Ruciński <rafal_rucinski@poczta.fm>
2020-05-12 22:32:45 +0100

    Add project files.

---
+++ Shared/ChildComoponentBase.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Brello.Shared
+{
+    public class ChildComoponentBase : ComponentBase
+    {
+        
+    [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        protected bool DarkThemeOn;
+        protected string AlertTheme => DarkThemeOn ? "dark" : "light";
+
+        protected override void OnInitialized()
+        {
+            DarkThemeOn = true;
+        }
+
+    }
+
+}
