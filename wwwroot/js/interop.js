264e7fdb645654f3d9cd0dbfa44001bfec5b65e4
Rafał Ruciński <rafal_rucinski@poczta.fm>
2020-05-14 21:22:59 +0100

    ok

---
+++ wwwroot/js/interop.js
@@ -0,0 +1,15 @@
+﻿function createAlert() {
+    alert("Alert from JS");
+}
+
+function createPrompt(question) {
+    return prompt(question);
+}
+
+function setElementTextById(id, text) {
+    document.getElementById(id).innerText = text;
+}
+
+function focusJS(element) {
+    element.focus();
+}
